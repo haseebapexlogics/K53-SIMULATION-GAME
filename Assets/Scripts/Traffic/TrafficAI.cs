302172c45a113546c07ef2de8b56bb6ffd6e7a82
haseebapexlogics <haseebapexlogics@gmail.com>
2024-06-05 03:14:29 -0700

    401

--- Assets/Scripts/Traffic/TrafficAI.cs
+++ Assets/Scripts/Traffic/TrafficAI.cs
@@ -1,74 +1,16 @@
-<<<<<<< Updated upstream
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using SWS;
-
-public class TrafficAI : MonoBehaviour
-{
-    public bool Movement = false;
-
-    public Transform RayCastInitPoint;
-
-
-    RaycastHit hit;
-
-
-
-    void CheckNearbyVehicles()
-    {
-        if (Physics.Raycast(RayCastInitPoint.position, RayCastInitPoint.transform.forward, out hit, 25f))
-        {
-
-            Debug.DrawRay(RayCastInitPoint.position, transform.forward, Color.blue);
-
-            if (!Movement)
-            {
-                this.gameObject.GetComponent<splineMove>().Pause();
-            }
-            else if (Movement)
-            {
-                this.gameObject.GetComponent<splineMove>().Resume();
+using System.Collections;using System.Collections.Generic;using UnityEngine;using SWS;public class TrafficAI : MonoBehaviour{    public bool Movement = false;    public Transform RayCastInitPoint;    RaycastHit hit;    void CheckNearbyVehicles()    {        if (Physics.Raycast(RayCastInitPoint.position, RayCastInitPoint.transform.forward, out hit, 25f))        {            Debug.DrawRay(RayCastInitPoint.position, transform.forward, Color.blue);            if (hit.transform.gameObject.CompareTag("TrafficVehicle") || hit.transform.root.CompareTag("Player"))            {                Debug.Log("Hit = " + hit.transform.name);                Debug.Log("Hit dist = " + hit.distance);                if (hit.distance <= 5)                {                    this.gameObject.GetComponent<splineMove>().Pause();                }                else                {                    this.gameObject.GetComponent<splineMove>().Resume();                }            }            else            {                if (!Movement)                {                    this.gameObject.GetComponent<splineMove>().Pause();                }                else if (Movement)                {                    this.gameObject.GetComponent<splineMove>().Resume();                }
+                //this.gameObject.GetComponent<splineMove>().Resume();
 
             }
 
-
-
-            if (hit.transform.gameObject.CompareTag("TrafficVehicle")  || hit.transform.root.CompareTag("Player"))
-            {
-                Debug.Log("Hit = " + hit.transform.name);
-                Debug.Log("Hit dist = "+ hit.distance);
-                if (hit.distance <= 5)
-                {
-                    this.gameObject.GetComponent<splineMove>().Pause();
-                }
-                else
-                {
-                    this.gameObject.GetComponent<splineMove>().Resume();
-                }
-            }
-            else
-            {
-                this.gameObject.GetComponent<splineMove>().Resume();
-
-            }
-            //else if (hit.transform.gameObject.CompareTag("TrafficVehicle") == false && hit.distance > 5 || Movement || hit.transform.root.CompareTag("Player") == false)
+            //if (!Movement)
+            //{
+            //    this.gameObject.GetComponent<splineMove>().Pause();
+            //}
+            //else if (Movement)
             //{
             //    this.gameObject.GetComponent<splineMove>().Resume();
+
             //}
-        }
 
-    }
-
-    private void Update()
-    {
-        CheckNearbyVehicles();
-
-    }
-}
-=======
-using System.Collections;using System.Collections.Generic;using UnityEngine;using SWS;public class TrafficAI : MonoBehaviour{    public bool Movement = false;    public Transform RayCastInitPoint;    RaycastHit hit;    void CheckNearbyVehicles()    {        if (Physics.Raycast(RayCastInitPoint.position, Vector3.forward, out hit))        {            Debug.Log("hit " + hit.transform.name);            Debug.DrawRay(RayCastInitPoint.position, transform.forward, Color.blue);
-
-
-            if ((hit.transform.gameObject.CompareTag("TrafficVehicle") && hit.distance <= 10) || !Movement || hit.transform.root.CompareTag("Player"))            {                this.gameObject.GetComponent<splineMove>().Pause();            }            else if ((hit.transform.gameObject.CompareTag("TrafficVehicle") == false && hit.distance > 10) || Movement )            {                this.gameObject.GetComponent<splineMove>().Resume();            }        }    }    private void Update()    {        CheckNearbyVehicles();    }}
->>>>>>> Stashed changes
+        }    }    private void Update()    {        CheckNearbyVehicles();    }}
--- Assets/Scripts/Traffic/TrafficActivator.cs
+++ Assets/Scripts/Traffic/TrafficActivator.cs
@@ -1,28 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using SWS;
+using System.Collections;using System.Collections.Generic;using UnityEngine;using SWS;public class TrafficActivator : MonoBehaviour{    private void OnTriggerEnter(Collider other)    {        if (other.transform.root.CompareTag("Player"))        {            for (int i = 0; i < GetComponentInParent<TrafficSignalC>().TrafficCars.Length; i++)            {                GetComponentInParent<TrafficSignalC>().TrafficCars[i].GetComponent<TrafficAI>().Movement = true;                GetComponentInParent<TrafficSignalC>().TrafficCars[i].GetComponent<splineMove>().StartMove();            }            this.GetComponent<BoxCollider>().enabled = false;        }
 
-public class TrafficActivator : MonoBehaviour
-{
-    private void OnTriggerEnter(Collider other)
-    {
-
-        if (other.transform.root.CompareTag("Player"))
-        {
-<<<<<<< Updated upstream
-            //Debug.Log("Here");
-=======
-            Debug.Log("Here");
-            for (int i = 0; i < GetComponentInParent<TrafficSignalC>().TrafficCars.Length; i++)
-            {
-                GetComponentInParent<TrafficSignalC>().TrafficCars[i].GetComponent<TrafficAI>().Movement = true;
-                GetComponentInParent<TrafficSignalC>().TrafficCars[i].GetComponent<splineMove>().StartMove();
-            }
-            GetComponent<BoxCollider>().enabled = false;
->>>>>>> Stashed changes
-        }
-        
-
-    }
-}
+    }}
