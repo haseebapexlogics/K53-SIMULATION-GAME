7778e356746a36f558195b77b47c1541e2cecf29
haseebapexlogics <haseebapexlogics@gmail.com>
2024-06-10 22:02:35 -0700

    806

--- Assets/Scripts/Traffic Signals/ZebraCheckerC.cs
+++ Assets/Scripts/Traffic Signals/ZebraCheckerC.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class ZebraCheckerC : MonoBehaviour
 {
+    public bool sign = false;
+    public bool rule = false;
+
+    public string FollowedString;
+    public string NotFollowedString;
+
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.root.CompareTag("Player"))
         {
             if (GetComponentInParent<TrafficSignalC>().Signalindex == 1 || GetComponentInParent<TrafficSignalC>().Signalindex == 2)
             {
-                AlertHandler.Instance.OnShowPopUp("Road Rule not Followed", Color.red);
+                AlertHandler.Instance.OnShowPopUp(NotFollowedString, Color.red);
             }
             else if (GetComponentInParent<TrafficSignalC>().Signalindex == 3)
             {
-                AlertHandler.Instance.OnShowPopUp("Road Rule Followed", Color.green);
-                GameManager.Instance.NumberOfRulesFollowed++;
+                if (sign)
+                {
+                    AlertHandler.Instance.OnShowPopUp(FollowedString, Color.green);
+                    GameManager.Instance.NumberOfSignsFollowed++;
+
+                }
+                else if (rule)
+                {
+                    AlertHandler.Instance.OnShowPopUp(FollowedString, Color.green);
+                    GameManager.Instance.NumberOfRulesFollowed++;
+                }
+                
             }
 
             this.GetComponent<BoxCollider>().enabled = false;
